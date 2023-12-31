c1069a02f8d962909c678eeb2f98c605968f322c
Naoki Yamamoto <mtbk4919naoki@gmail.com>
2023-12-31 17:18:29 +1100

    refactor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const ROW=4, COL=4, SIZ=120;
   const [table, setTable] = useState([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
   const [isEnd, setIsEnd] = useState(false);
   const [times, setTimes] = useState(0);
@@ -18,13 +19,13 @@ function App() {
     newTable[index]++;
 
     // 上にセルがある場合
-    if(index > 3) newTable[index-4]++;
+    if(index > (COL-1)) newTable[index-ROW]++;
     // 右にセルがある場合
-    if( (index+1) % 4 !== 0) newTable[index+1]++;
+    if( (index+1) % ROW !== 0) newTable[index+1]++;
     // 下にセルがある場合
-    if(index < 12) newTable[index+4]++;
+    if(index < (ROW*COL-COL)) newTable[index+ROW]++;
     // 左にセルがある場合
-    if( (index) % 4 !== 0) newTable[index-1]++;
+    if( (index) % ROW !== 0) newTable[index-1]++;
 
     const judgement = newTable.reduce((acc,cur)=>(cur%2!==0)?acc-1:acc,newTable.length)
     if(judgement <= 0) setIsEnd(true);
@@ -35,7 +36,7 @@ function App() {
     <div className={isEnd?'app end':'app'}>
       <div className='howto'><h2>Flip all panels</h2><p>If you tap a panel, tapped panel and its neighbor panels will be fliped.</p><p>全てのパネルをひっくり返そう！</p></div>
       <div className="grid">
-        {table.map((item,index)=><div className={table[index]%2===0?'cell even':'cell odd'} onClick={()=>{turn(index)}} style={{backgroundPosition:`${index%4*-120}px ${Math.floor(index/4)*-120}px`}}></div>)}
+        {table.map((item,index)=><div className={table[index]%2===0?'cell even':'cell odd'} onClick={()=>{turn(index)}} style={{backgroundPosition:`${index%COL*-SIZ}px ${Math.floor(index/ROW)*-SIZ}px`}}></div>)}
       </div>
       {isEnd && <div className='congrat'><h1>Congraturations!</h1><p>You turned panels {times} times.</p><p>{times}回 パネルをひっくり返しました。</p></div>}
     </div>
